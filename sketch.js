f73700c160eee6da34185d47dadee41222db46bb
MathManiacc <73884571+MathManiacc@users.noreply.github.com>
2020-12-31 10:59:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 //Create variables here
 var dog, dogImage, happyDog, database, foodS, foodStock;
+var database;
 
 function preload()
 {
@@ -9,8 +10,9 @@ function preload()
 
 function setup() {
 	createCanvas(500, 500);
-  dog = createSprite(250, 400, 30, 30);
+  dog = createSprite(250, 350, 30, 30);
   dog.addImage(dogImage);
+  dog.scale = 0.3;
 
   database = firebase.database();
   foodStock = database.ref('Food');
@@ -22,10 +24,10 @@ function draw() {
   background(46, 139, 87);
   if(keyWentDown(UP_ARROW)){
     writeStock(foodS);
-    dog.addImage(dogHappy);
+    dog.addImage(happyDog);
   }
   drawSprites();
-  textSize(8);
+  textSize(20);
   stroke(0);
   fill(0);
   text("Food: " + foodS, 50, 50);
@@ -37,6 +39,14 @@ function readStock(data){
 }
 
 function writeStock(x){
+if (x<=0){
+  x = 0;
+} else{
+  x -= 1;
+}
+
+
+
   database.ref('/').update({
     Food:x
   })
